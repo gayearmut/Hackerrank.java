83e5af2af2409fe4ba22d4b020f5b99d013e7af6
Gaye Armut <57320188+gayearmut@users.noreply.github.com>
2023-01-09 21:28:31 +0300

    Create Java_Stdin_and_Stdout_I.java

---
+++ Java_Stdin_and_Stdout_I.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	 
+     int firstNumber,secondNumber,thirdNumber;
+     Scanner scanner = new Scanner(System.in);
+     
+     firstNumber=scanner.nextInt();
+     System.out.println(firstNumber);
+     
+     secondNumber=scanner.nextInt();
+     System.out.println(secondNumber);
+     
+     thirdNumber=scanner.nextInt();
+     System.out.println(thirdNumber);
+     
+  
+     
+    }
+}
